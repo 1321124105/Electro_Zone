a80720414e0b945283299d383df2877b16e7c3b6
SAUL IVAN GARCIA GARCIA <saul_1321124105@uptecamac.edu.mx>
2023-05-26 09:27:20 -0600

    Proyecto Estancia

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/404',function(){
+    return view('404');
+});
+
+Route::get('/cart',function(){
+    return view('shop-filter');
+});
